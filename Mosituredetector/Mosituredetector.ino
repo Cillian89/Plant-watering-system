c901fabe7d0e0559a509047fb7bce40b98c5cf5f
Cillian89 <s00141486@atu.ie>
2025-03-12 18:15:27 +0000

    Mosituredetector.ino

--- Mosituredetector/Mosituredetector.ino
+++ Mosituredetector/Mosituredetector.ino
@@ -1,15 +1,8 @@
-#define MOISTURE_SENSOR A0  // Sensor connected to A0
 
-void setup() {
-  Serial.begin(9600); // Start serial monitor
+int readMoistureLevel() {
+  int moistureValue = analogRead(moistureSensor); // Read moisture level
+  Serial.print("Moisture Level: "); 
+  Serial.println(moistureValue);  // Print moisture value
+  return moistureValue; // Return value to loop()
 }
-
-void loop() {
-  int moistureValue = analogRead(MOISTURE_SENSOR); // Read sensor value
-  Serial.print("Soil Moisture Value: ");
-  Serial.println(moistureValue); // Print value in Serial Monitor
-
-  delay(1000); // Wait 1 second
-}
-
 
