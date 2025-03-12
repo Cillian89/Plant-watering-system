4fe8aee72441695454485b4a93a280659ff3998e
Cillian89 <s00141486@atu.ie>
2025-03-12 18:08:54 +0000

    workingmodel.ino

--- workingmodel/workingmodel.ino
+++ workingmodel/workingmodel.ino
@@ -1,21 +1,22 @@
-int moistureSensor = A0;    // Pin for moisture sensor
-int relayPin = 7;           // Pin for the relay (connected to D7 on Base Shield)
-int moistureValue = 0;      // Variable to store sensor value
+int moistureSensor = A0;  // Moisture sensor connected to A0
+int relayPin = 3;         // Relay module connected to pin 3
+int moistureThreshold = 500; // Threshold for water dispensing
 
 void setup() {
-  pinMode(relayPin, OUTPUT);  // Set relay pin as output
-  Serial.begin(9600);         // Start serial communication for debugging
+  pinMode(relayPin, OUTPUT); // Output pin for relay
+  Serial.begin(9600);        // Start Serial Monitor for debugging
 }
 
 void loop() {
-  moistureValue = analogRead(moistureSensor); // Read moisture sensor value
-  Serial.println(moistureValue);  // Print value for debugging
+  int moistureValue = analogRead(moistureSensor); // Read moisture level
+  Serial.print("Moisture Level: "); 
+  Serial.println(moistureValue);  // Print moisture value
 
-  if (moistureValue <= 500) {  // Threshold for dry soil
-    digitalWrite(relayPin, HIGH); // Turn on pump
+  if (moistureValue > moistureThreshold) { 
+    digitalWrite(relayPin, HIGH); // Turn ON water pump if soil is dry
   } else {
-    digitalWrite(relayPin, LOW);  // Turn off pump
+    digitalWrite(relayPin, LOW);  // Turn OFF water pump if soil is moist
   }
-
-  delay(1000);  // Wait for 1 second before the next reading
+  
+  delay(1000); // Wait before the next reading
 }
